a88b338fbcaffaa703d9f2d7672e7c50b827c0ab
Reniel Ocampo <renielxocampo@gmail.com>
2021-11-02 21:49:25 -0700

    remove trailing whitespaces

--- script.js
+++ script.js
@@ -17,41 +17,41 @@ pages.forEach((page, index) => {
     startX = event.touches[0].pageX
     startY = event.touches[0].pageY
   })
-  
+
   page.addEventListener("touchmove", (event) => {
     if (!isDragging) {
       return
     }
     console.log('touchmove')
-    
+
     const currX = event.touches[0].pageX
     const currY = event.touches[0].pageY
     const xDiff = Math.abs(startX - currX)
     const yDiff = Math.abs(startY - currY)
-    
+
     if (yDiff >= xDiff && !isTranslating) {
       // user is scrolling tab
       verticalScroll = true
     }
-    
+
     if (!verticalScroll) {
       event.preventDefault()
       isTranslating = true
 
       const distTraveled = currX - startX
       currTranslate = prevTranslate + distTraveled
-      
+
       container.style.transform = `translate(${currTranslate}px)`
     }
   })
-  
+
   page.addEventListener("touchend", (event) => {
     console.log('touchend')
     isDragging = false
     verticalScroll = false
     isTranslating = false
     const distTraveled = currTranslate - prevTranslate
-    
+
     if (distTraveled < -50 && index + 1 < pages.length) {
       currTranslate = 0 + (index + 1) * -page.offsetWidth
     } else if (distTraveled > 50 && index > 0) {
